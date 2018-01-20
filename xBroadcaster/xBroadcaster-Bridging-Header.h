f334c688ccdb394485b4082602c1f71c01c8aa67
Douglas Adams <douglas.adams@me.com>
2018-01-20 14:22:26 -0500

    Needed for reorg

---
+++ xBroadcaster/xBroadcaster-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  xBroadcaster-BridgingHeader.h
+//  xBroadcaster
+//
+//  Created by Douglas Adams on 1/20/18.
+//  Copyright © 2018 Douglas Adams. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "GCDAsyncUdpSocket.h"
+#import "GCDAsyncSocket.h"
+
